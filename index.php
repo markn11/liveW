c15560594ea03988f7560d7fabeb6a4199d6311d
markn11 <m-niles@onu.edu>
2017-04-05 08:20:01 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<!DOCTYPE html>
+<html>
+     
+<body>
+    <style>
+       body {
+    background-color: orange;
+}</style>
+<h1>My Net Centic Projects </h1>
+<h1>By Mark Niles </h1>
+
+
+ <img src="world.png" alt=""/>  <div </div>
+<a href="http://localhost/innovhello/index.php"> Innovative Hello World </a> 
+<div </div><a href="http://localhost/Helloworld1/index.php">Hello World </a> </div>       
+<img src="desert.png" alt=""/>
+<div </div><a href="http://localhost/playingwithcss/index.php">Playing with CSS</a>
+<div </div>    
+<img src="coke1.png" alt=""height="300" width="200"/>
+<div </div> <a href="http://localhost/advertisment/index.php">Mimic a Print Advertisment</a>     
+<div </div>
+<img src="cale.png" alt=""/>
+<div </div> <a href="http://localhost/fcalendar/index.php">Calendar</a>
+<div </div>
+<img src="play_full_color.png" alt="" height="300" width="200"/>
+<div </div><a href="http://localhost/javaplay/index.php">JavaScript Play</a>
+
+
+
+</body>
+</html>
